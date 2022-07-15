24c4ec04fd20ca4a687a5065eadaa444468bf5a7
Ashley <49788162+Yodawgz0@users.noreply.github.com>
2022-07-16 02:04:28 +0530

    Communcation with phone

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import socket
 
-HOST = "127.0.0.1"  # The server's hostname or IP address
+print("hi")
+
+
+
+HOST = "192.168.1.103"  # The server's hostname or IP address
 PORT = 80  # The port used by the server
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
+
+
+
 s.sendall(b"Hello, world")
 data = s.recv(1024)
 
