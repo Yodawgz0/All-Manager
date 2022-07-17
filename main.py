ce785e12f928a4ceca435304fab00f26386bc17b
Ashley <49788162+Yodawgz0@users.noreply.github.com>
2022-07-17 23:14:19 +0530

    added Handlers in Python init

--- main.py
+++ main.py
@@ -1,18 +1,42 @@
+from ast import arg
+from curses import flash
 import socket
-
-print("hi")
-
-
-
-HOST = "192.168.1.103"  # The server's hostname or IP address
-PORT = 80  # The port used by the server
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((HOST, PORT))
+from threading import Thread
+from time import sleep
+from flask import Flask, jsonify, request
 
 
+def TCP_Handler(arg):
 
-s.sendall(b"Hello, world")
-data = s.recv(1024)
+    HOST = "192.168.1.103"  # The server's hostname or IP address
+    PORT = 80  # The port used by the server
 
-print(f"Received {data!r}")
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((HOST, PORT))
+
+
+
+    s.sendall(b"Hello, world")
+    data = s.recv(1024)
+
+    print(f"Received {data!r}")
+    
+def API_Handler(arg):
+    app = Flask(__name__)
+    @app.route('/', methods = ['GET', 'POST'])
+    def request_handler():
+        if(request.method == 'GET'):
+    
+            data = "hello world"
+            return jsonify({'data': data})
+    
+
+
+if __name__ == "__main__":
+    thread = Thread(target = TCP_Handler, args = (10, ))
+    thread = Thread(target = API_Handler, args = (10, ))
+    thread.start()
+    thread.start()
+    thread.join()
+    thread.join()
+    print("thread finished...exiting")
