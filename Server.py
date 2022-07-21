5f6f0a1e6dc4f1dae5789457d25ef89e390c8842
Ashley <49788162+Yodawgz0@users.noreply.github.com>
2022-07-22 00:02:29 +0530

    Multithreading added

---
+++ Server.py
@@ -0,0 +1,58 @@
+import socket
+from threading import Thread
+from time import sleep
+from flask import Flask, jsonify, request
+import json
+
+connectionestd = True
+hostname=socket.gethostname()   
+IPAddr=socket.gethostbyname(hostname)
+
+app = Flask(__name__)
+@app.route('/', methods = ['GET', 'POST'])
+def request_handler():
+    if(request.method == 'GET'):
+        data = "hello world"
+        return jsonify({'data': data})
+
+
+##with open('dataStore.json', 'r') as f:
+##    people = json.load(f)['people']
+
+def TCP_Handler(arg):
+    print(">>>>>>>>>>>>> Starting TCP Handler....\n")
+    HOST = IPAddr  # The server's hostname or IP address
+    PORT = 4000  # The port used by the server
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    while(1):
+        try:
+            s.connect((HOST, PORT))
+        except:
+            print("Please Check if the IP is Hosted is not!!\n")
+            print("##############CONFIRM###############\n")
+            confrm_host = input("Y for checked / N for quit")
+            if confrm_host.lower() == 'y':
+                continue
+            else:
+                break
+        print(f">>>>>>>>>>>>> Connection estd on the IP {IPAddr}....\n")
+
+        while(connectionestd):
+            print(">>>>>>>>>>>>> Sending Data\n")
+            s.sendall(b"Hello, world")
+            data = s.recv(1024)
+
+            print(f">>>>>>>>>>>>> Received {data!r}\n")
+            sleep(0.5)
+
+
+if __name__ == "__main__":
+
+    thread = Thread(target = TCP_Handler, args = (10, ))
+    thread.start()
+    print("starting API Handler\n")
+    app.debug= False
+    app.run(IPAddr, port="5000")
+    thread.join()
+    print("thread finished...exiting\n")
