15d8dbe8a105ec6433f9fcf07b274632a06ebb81
Ashley <49788162+Yodawgz0@users.noreply.github.com>
2022-07-27 22:54:19 +0530

    adding navigation

--- managerAsh/App.js
+++ managerAsh/App.js
@@ -1,15 +1,22 @@
-import { StyleSheet, View } from "react-native";
 import React from "react";
 import FirstPage from "./src/screens/FirstPage";
+import MainPage from "./src/screens/MainPage";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 const App = () => {
+  const Stack = createNativeStackNavigator();
   return (
-    <View>
-      <FirstPage />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="LoginPage"
+        screenOptions={{ headerShown: false }}
+      >
+        <Stack.Screen name="LoginPage" component={FirstPage} />
+        <Stack.Screen name="mainPage" component={MainPage} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
-
-const styles = StyleSheet.create({});
 
 export default App;
