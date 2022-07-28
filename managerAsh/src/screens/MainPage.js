b237e8ea9bbf4ceaa8e14f71d1a2c9a9f44aa13b
Ashley <49788162+Yodawgz0@users.noreply.github.com>
2022-07-28 22:16:05 +0530

    adding props of button

--- managerAsh/src/screens/MainPage.js
+++ managerAsh/src/screens/MainPage.js
@@ -2,18 +2,20 @@ import { Text, View, ImageBackground } from "react-native";
 import { MainPagestyle } from "../stylesFiles/MainPageStyles";
 import React from "react";
 import MainPageComponents from "./MainPageComponents";
-const MainPage = () => {
+const MainPage = ({ navigation }) => {
   return (
     <View>
       <ImageBackground
-        source={require("../../assets/back_omg.jpg")}
+        source={require("../../assets/mainPag_back.jpg")}
         style={MainPagestyle.back_image}
         resizeMode="cover"
       >
         <View style={MainPagestyle.mainSheetStyle}>
           <Text style={MainPagestyle.textStyle}>Select!{"\n"}The Option </Text>
         </View>
-        <MainPageComponents />
+        <MainPageComponents title="Text Reader" navigation={navigation} />
+        <MainPageComponents title="Play Music" />
+        <MainPageComponents title="Items Tracker" />
       </ImageBackground>
     </View>
   );
