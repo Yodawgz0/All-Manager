331a99c2ba9fb827c9e15920e583d3b5a89509fd
Ashley <49788162+Yodawgz0@users.noreply.github.com>
2022-08-04 23:14:01 +0530

    adding page in stack

--- managerAsh/App.js
+++ managerAsh/App.js
@@ -4,6 +4,7 @@ import MainPage from "./src/screens/MainPage";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import TextReaderScreen from "./src/screens/TextReaderScreen";
+import MusicPlayerPage from "./src/screens/MusicPlayerPage";
 
 const App = () => {
   const Stack = createNativeStackNavigator();
@@ -16,6 +17,7 @@ const App = () => {
         <Stack.Screen name="LoginPage" component={FirstPage} />
         <Stack.Screen name="mainPage" component={MainPage} />
         <Stack.Screen name="TextReaderScreen" component={TextReaderScreen} />
+        <Stack.Screen name="MusicPlayerPage" component={MusicPlayerPage} />
       </Stack.Navigator>
     </NavigationContainer>
   );
